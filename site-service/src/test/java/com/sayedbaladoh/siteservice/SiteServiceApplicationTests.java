fefd4a5d5fb5f33eb93f58292ea308d44bb1f991
sekharbabug <sekhar.babu260@gmail.com>
2023-07-28 18:08:36 +0400

    Update SiteServiceApplicationTests.java

--- site-service/src/test/java/com/sayedbaladoh/siteservice/SiteServiceApplicationTests.java
+++ site-service/src/test/java/com/sayedbaladoh/siteservice/SiteServiceApplicationTests.java
@@ -1,15 +1,14 @@
 package com.sayedbaladoh.siteservice;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
 public class SiteServiceApplicationTests {
 
-	@Test
+	//@Test
 	public void contextLoads() {
 	}
 
